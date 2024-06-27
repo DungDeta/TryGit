354cea243c3b4ddd5f0bf90b255c1a7adedd311e
DungDeta <22010492@st.phenikaa-uni.edu.vn>
2024-06-27 10:56:14 +0700

    Binary Tree Struct

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,9 @@
-use std::string;
-fn main(){
-    println!("Hello World");
+use std::cmp::Ordering;
+use std::ops::Deref;
+pub struct  BT<T>
+where T: Ord,
+{
+    value: Option<T>,
+    left: Option<Box<BT<T>>>,
+    right: Option<Box<BT<T>>>,
 }
